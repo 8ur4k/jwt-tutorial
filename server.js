7e3387474cab1a33a6549ea2539a1dc916666d3d
8ur4k <ilgenciburak@gmail.com>
2024-10-11 01:06:08 +0300

    Implement user signup and get users endpoints

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
+const users = [];
 
 const posts = [
   {
@@ -12,8 +17,25 @@ const posts = [
   },
 ];
 
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
 app.get("/posts", (req, res) => {
   res.json(posts);
 });
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { username: req.body.username, password: hashedPassword };
+    users.push(user);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get("/login", (req, res) => {});
 
 app.listen(3000);
