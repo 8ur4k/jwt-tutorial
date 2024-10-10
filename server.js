edfffdfd9f37a8a55c2883431e545f3f785478a6
8ur4k <ilgenciburak@gmail.com>
2024-10-10 23:36:21 +0300

    Create example posts data

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
 
+const posts = [
+  {
+    username: "Kyle",
+    title: "Post 1",
+  },
+  {
+    username: "Jim",
+    title: "Post 2",
+  },
+];
+
 app.listen(3000);
