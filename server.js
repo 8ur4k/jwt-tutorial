82ebeecd21812afe263197cca4b470227dd5de0b
8ur4k <ilgenciburak@gmail.com>
2024-10-13 19:46:22 +0300

    Implement auth middleware

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.username));
 });
 
 app.post("/register", async (req, res) => {
@@ -60,5 +60,17 @@ app.post("/login", async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
